5f7243b140de72c39631a9c72cb24d4b085da5e3
TheCardBoxMan <67300002+TheCardBoxMan@users.noreply.github.com>
2024-02-07 13:30:29 -0800

    editting

--- picarx/linefollow.py
+++ picarx/linefollow.py
@@ -12,71 +12,23 @@
         and the background gray value.
 
 '''
-from picarx import picarx_improved
+import picarx_improved as pc
 from time import sleep
+px = pc.Picarx()
 
-px = picarx_improved.Picarx()
-# px = Picarx(grayscale_pins=['A0', 'A1', 'A2'])
+class Sensor:
+    def __init__(self):
+        self.adc_1 = ADC(channel=1)
+        self.adc_2 = ADC(channel=2)
+        self.adc_3 = ADC(channel=3)
 
-# Please run ./calibration/grayscale_calibration.py to Auto calibrate grayscale values
-# or manual modify reference value by follow code
-# px.set_line_reference([1400, 1400, 1400])
-
-current_state = None
-px_power = 10
-offset = 20
-last_state = "stop"
-
-def outHandle():
-    global last_state, current_state
-    if last_state == 'left':
-        px.set_dir_servo_angle(-30)
-        px.backward(10)
-    elif last_state == 'right':
-        px.set_dir_servo_angle(30)
-        px.backward(10)
-    while True:
-        gm_val_list = px.get_grayscale_data()
-        gm_state = get_status(gm_val_list)
-        print("outHandle gm_val_list: %s, %s"%(gm_val_list, gm_state))
-        currentSta = gm_state
-        if currentSta != last_state:
-            break
-    sleep(0.001)
-
-def get_status(val_list):
-    _state = px.get_line_status(val_list)  # [bool, bool, bool], 0 means line, 1 means background
-    if _state == [0, 0, 0]:
-        return 'stop'
-    elif _state[1] == 1:
-        return 'forward'
-    elif _state[0] == 1:
-        return 'right'
-    elif _state[2] == 1:
-        return 'left'
-
+    def read_sensor(self):
+        # Poll the three ADC structures and put their outputs into a list
+        sensor_readings = px.get_grayscale_data(self)
+        return sensor_readings
+    
 if __name__=='__main__':
-    try:
-        while True:
-            gm_val_list = px.get_grayscale_data()
-            gm_state = get_status(gm_val_list)
-            print("gm_val_list: %s, %s"%(gm_val_list, gm_state))
-
-            if gm_state != "stop":
-                last_state = gm_state
-
-            if gm_state == 'forward':
-                px.set_dir_servo_angle(0)
-                px.forward(px_power) 
-            elif gm_state == 'left':
-                px.set_dir_servo_angle(offset)
-                px.forward(px_power) 
-            elif gm_state == 'right':
-                px.set_dir_servo_angle(-offset)
-                px.forward(px_power) 
-            else:
-                outHandle()
-    finally:
-        px.stop()
-        print("stop and exit")
-        sleep(0.1)
+    print("Line Following Start")
+    my_sensor = Sensor()
+    readings = my_sensor.read_sensor()
+    print("Sensor readings:", readings)
